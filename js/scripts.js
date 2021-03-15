562fbcd32b4d31122b4a574337dff226b9598d1a
geomio <geoff.millner22@gmail.com>
2021-03-15 11:59:00 -0700

    added const js for form side a b and c

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("form#triform").submit(function(event) {
+    const sideA = parseInt($('input#sideA').val());
+    const sideB = parseInt($('input#sideB').val());
+    const sideC = parseInt($('input#sideC').val());
+
+  });
+});
