e1bcaa796453cddefd898775f1cb48ab5fc2dd27
geomio <geoff.millner22@gmail.com>
2021-03-16 11:36:58 -0700

    add js code functions to show diffrent triangle outputs

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,43 @@ $(document).ready(function() {
     const sideB = parseInt($('input#sideB').val());
     const sideC = parseInt($('input#sideC').val());
 
+    if (sideA === sideB && sideC === sideA){
+      $("#equilateral").show();
+      $("#isosceles").hide();
+      $("#scalene").hide();
+    }else if(sideA === sideC && sideA !== sideB){
+      $("#isosceles").show();
+      $("#equilateral").hide();
+      $("#scalene").hide();
+    }else if(sideB === sideA && sideB !== sideC){
+      $("#isosceles").show();
+      $("#equilateral").hide();
+      $("#scalene").hide();
+    }else if(sideB === sideC && sideB !== sideA){
+      $("#isosceles").show();
+      $("#equilateral").hide();
+      $("#scalene").hide();
+    }else if(sideC === sideA && sideC !== sideB){
+      $("#isosceles").show();
+      $("#equilateral").hide();
+      $("#scalene").hide();
+    }else if(sideC === sideB && sideC !== sideA){
+      $("#isosceles").show();
+      $("#equilateral").hide();
+      $("#scalene").hide();
+    }else if(sideA !== sideB && sideA !== sideC){
+      $("#scalene").show();
+      $("#equilateral").hide();
+      $("#isosceles").hide();
+    }else if(sideB !== sideA && sideB !== sideC){
+      $("#scalene").show();
+      $("#equilateral").hide();
+      $("#isosceles").hide();
+    }else if(sideC !== sideA && sideC !== sideB){
+      $("#scalene").show();
+      $("#equilateral").hide();
+      $("#isosceles").hide();
+    }
+    event.preventDefault();
   });
 });
