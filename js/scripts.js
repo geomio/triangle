90242b7ce7929646d758c4376e5a30e6fab166dd
geomio <geoff.millner22@gmail.com>
2021-03-16 12:04:04 -0700

    fix css and have js swap out output answers

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,41 @@ $(document).ready(function() {
     const sideC = parseInt($('input#sideC').val());
 
     if (sideA === sideB && sideC === sideA){
-      $("#equilateral").show();
-      $("#isosceles").hide();
-      $("#scalene").hide();
+      $(".equilateral").show();
+      $(".isosceles").hide();
+      $(".scalene").hide();
     }else if(sideA === sideC && sideA !== sideB){
-      $("#isosceles").show();
-      $("#equilateral").hide();
-      $("#scalene").hide();
+      $(".isosceles").show();
+      $(".equilateral").hide();
+      $(".scalene").hide();
     }else if(sideB === sideA && sideB !== sideC){
-      $("#isosceles").show();
-      $("#equilateral").hide();
-      $("#scalene").hide();
+      $(".isosceles").show();
+      $(".equilateral").hide();
+      $(".scalene").hide();
     }else if(sideB === sideC && sideB !== sideA){
-      $("#isosceles").show();
-      $("#equilateral").hide();
-      $("#scalene").hide();
+      $(".isosceles").show();
+      $(".equilateral").hide();
+      $(".scalene").hide();
     }else if(sideC === sideA && sideC !== sideB){
-      $("#isosceles").show();
-      $("#equilateral").hide();
-      $("#scalene").hide();
+      $(".isosceles").show();
+      $(".equilateral").hide();
+      $(".scalene").hide();
     }else if(sideC === sideB && sideC !== sideA){
-      $("#isosceles").show();
-      $("#equilateral").hide();
-      $("#scalene").hide();
+      $(".isosceles").show();
+      $(".equilateral").hide();
+      $(".scalene").hide();
     }else if(sideA !== sideB && sideA !== sideC){
-      $("#scalene").show();
-      $("#equilateral").hide();
-      $("#isosceles").hide();
+      $(".scalene").show();
+      $(".equilateral").hide();
+      $(".isosceles").hide();
     }else if(sideB !== sideA && sideB !== sideC){
-      $("#scalene").show();
-      $("#equilateral").hide();
-      $("#isosceles").hide();
+      $(".scalene").show();
+      $(".equilateral").hide();
+      $(".isosceles").hide();
     }else if(sideC !== sideA && sideC !== sideB){
-      $("#scalene").show();
-      $("#equilateral").hide();
-      $("#isosceles").hide();
+      $(".scalene").show();
+      $(".equilateral").hide();
+      $(".isosceles").hide();
     }
     event.preventDefault();
   });
